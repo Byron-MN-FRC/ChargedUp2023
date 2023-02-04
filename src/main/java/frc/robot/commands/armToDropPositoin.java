ea6603743694ea30b6800afb8ba1be9ba6f0cf18
thayne67 <thayne67@students.byron.k12.mn.us>
2023-02-04 13:03:30 -0800

    hjk.jhku

--- src/main/java/frc/robot/commands/armToDropPositoin.java
+++ src/main/java/frc/robot/commands/armToDropPositoin.java
@@ -55,6 +55,7 @@ public class armToDropPositoin extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        System.out.println("arm to drop positoin");
     }
 
     // Called once the command ends or is interrupted.
