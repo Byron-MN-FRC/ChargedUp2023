a475089f98473519f7a9d031b075580412dadb64
kmbrinkmann <kbrink29@students.byron.k12.mn.us>
2023-02-10 16:17:36 -0600

    undepricated some stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -205,11 +205,11 @@ thetaController.enableContinuousInput(-Math.PI, Math.PI);
            // Start at the origin facing the +X direction
            new Pose2d(0, 0, new Rotation2d(0)),
            // Pass through these two interior waypoints, making an 's' curve path
-          //  List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-          List.of(new Translation2d(1, 0)),
+           List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+          // List.of(new Translation2d(1, 0)),
            // End 3 meters straight ahead of where we started, facing forward
-           new Pose2d(5.2+.1778, -0.3810-.0762, new Rotation2d(0)),
-          // new Pose2d(3, 0, new Rotation2d(0)),
+          //  new Pose2d(5.2+.1778, -0.3810-.0762, new Rotation2d(0)),
+          new Pose2d(3, 0, new Rotation2d(0)),
            // Pass config
            config);
 
