177785ebfc0bc7c32a40ae7aa49496d3cea8ddab
daheinz <heinz.david@gmail.com>
2023-02-13 16:18:28 -0600

    fix imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,15 @@
 
 package frc.robot;
 
+import java.util.List;
+
 import org.photonvision.PhotonCamera;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -28,7 +35,6 @@ import frc.robot.commands.ClawGrab;
 import frc.robot.commands.ClawRelease;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.DriveLift;
-import frc.robot.commands.DriveToEncoder;
 import frc.robot.commands.DropAndRelease;
 import frc.robot.commands.ExtendArm;
 import frc.robot.commands.GrabAndRaise;
@@ -212,19 +218,18 @@ public class RobotContainer {
     // .setKinematics(DriveConstants.kDriveKinematics);
     // // Apply the voltage constraint
 
-    // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory =
-       TrajectoryGenerator.generateTrajectory(
-           // Start at the origin facing the +X direction
-           new Pose2d(0, 0, new Rotation2d(0)),
-           // Pass through these two interior waypoints, making an 's' curve path
-           List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-          // List.of(new Translation2d(1, 0)),
-           // End 3 meters straight ahead of where we started, facing forward
-          //  new Pose2d(5.2+.1778, -0.3810-.0762, new Rotation2d(0)),
-          new Pose2d(3, 0, new Rotation2d(0)),
-           // Pass config
-           config);
+    // An example trajectory to follow. All units in meters.
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        // List.of(new Translation2d(1, 0)),
+        // End 3 meters straight ahead of where we started, facing forward
+        // new Pose2d(5.2+.1778, -0.3810-.0762, new Rotation2d(0)),
+        new Pose2d(3, 0, new Rotation2d(0)),
+        // Pass config
+        config);
 
     // SwerveControllerCommand swerveControllerCommand =
     // new SwerveControllerCommand(
